d93203eada7c44a61f7e912b8eb50fafcfa2ade5
vitconkue <voan0935995700@gmail.com>
2022-03-18 04:46:19 +0700

    add buying price to real estate

---
+++ Infrastructure/Migrations/20220317194135_add-buying-price-to-real-estate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class addbuyingpricetorealestate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "BuyPrice",
+                table: "RealEstateAssets",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyPrice",
+                table: "RealEstateAssets");
+        }
+    }
+}
