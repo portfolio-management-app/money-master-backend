425d3147d1919d4310a2f32c750818ce03caec44
vitconkue <voan0935995700@gmail.com>
2022-04-21 13:47:00 +0700

    create a new cash asset when create portfolio

--- PublicAPI/Endpoints/Portfolio/Create.cs
+++ PublicAPI/Endpoints/Portfolio/Create.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Net.Http;
+using ApplicationCore.AssetAggregate.CashAggregate;
+using ApplicationCore.AssetAggregate.CashAggregate.DTOs;
 using ApplicationCore.InvestFundAggregate;
 using ApplicationCore.PortfolioAggregate;
 using Ardalis.ApiEndpoints;
@@ -14,11 +17,13 @@ namespace PublicAPI.Endpoints.Portfolio
     {
         private readonly IPortfolioService _portfolioService;
         private readonly IInvestFundService _investFundService;
+        private readonly ICashService _cashService;
 
-        public Create(IPortfolioService portfolioService, IInvestFundService investFundService)
+        public Create(IPortfolioService portfolioService, IInvestFundService investFundService, ICashService cashService)
         {
             _portfolioService = portfolioService;
             _investFundService = investFundService;
+            _cashService = cashService;
         }
 
         [HttpPost]
@@ -28,6 +33,17 @@ namespace PublicAPI.Endpoints.Portfolio
 
             var newPortfolio
                 = _portfolioService.CreatePortfolio(userId, request.Name, request.InitialCash, request.InitialCurrency);
+
+            var cashDto = new CashDto()
+            {
+                Amount = request.InitialCash,
+                CurrencyCode = request.InitialCurrency,
+                Description = "New cash when create portfolio",
+                InputDay = DateTime.Now,
+                Name = "Initial Cash",
+                PurchasePrice = request.InitialCash
+            };
+            _ = _cashService.CreateNewCashAsset(newPortfolio.Id, cashDto);
             return Ok(newPortfolio.Adapt<CreatePortfolioResponse>());
         }
     }
