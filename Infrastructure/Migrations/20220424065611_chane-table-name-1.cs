856fc45cb9194b0d3196f0f242ceb19ad8dd1aea
vitconkue <voan0935995700@gmail.com>
2022-04-24 14:01:39 +0700

    add unversioned migration files

---
+++ Infrastructure/Migrations/20220424065611_chane-table-name-1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class chanetablename1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SingleAssetTransactionType",
+                table: "SingleAssetTransactions",
+                newName: "SingleAssetTransactionTypes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SingleAssetTransactionTypes",
+                table: "SingleAssetTransactions",
+                newName: "SingleAssetTransactionType");
+        }
+    }
+}
