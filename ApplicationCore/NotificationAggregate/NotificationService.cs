f5796560796cffb6735527d5d664c5dab6f10e85
hoaxvo <dev.h1620@gmail.com>
2022-06-03 14:54:05 +0700

    handle dp injection in notification service

--- ApplicationCore/NotificationAggregate/NotificationService.cs
+++ ApplicationCore/NotificationAggregate/NotificationService.cs
@@ -1,43 +1,64 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ApplicationCore.PortfolioAggregate;
 
 
-namespace ApplicationCore.NotificationAggregate{
+namespace ApplicationCore.NotificationAggregate
+{
     public class NotificationService : IHostedService
     {
-       private readonly ILogger<NotificationService> logger;
+        private readonly ILogger<NotificationService> logger;
+        private readonly IServiceScopeFactory scopeFactory;
+
         private Timer timer;
-        private int number;
- 
-        public NotificationService(ILogger<NotificationService> logger)
+
+        public NotificationService(ILogger<NotificationService> logger, IServiceScopeFactory scopeFactory)
         {
             this.logger = logger;
+            this.scopeFactory = scopeFactory;
         }
- 
+
         public void Dispose()
         {
             timer?.Dispose();
         }
- 
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            timer = new Timer(o => {
-                Interlocked.Increment(ref number);
-                logger.LogInformation($"Printing the worker number {number}");
+            logger.LogInformation("Notification service is running !!");
+            timer = new Timer(o =>
+            {
+                ReadDb();
             },
             null,
             TimeSpan.Zero,
-            TimeSpan.FromSeconds(5));
- 
+            TimeSpan.FromMinutes(1));
+
             return Task.CompletedTask;
         }
- 
+
         public Task StopAsync(CancellationToken cancellationToken)
         {
+            logger.LogInformation("Notification service has been stopped!!");
             return Task.CompletedTask;
+        }
+
+        public void ReadDb()
+        {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var portfolioService = scope.ServiceProvider.GetRequiredService<IPortfolioService>();
+                var data = portfolioService.GetPortfolioList(1);
+                data.ForEach((item) =>
+                {
+                    logger.LogInformation($"Portfolio name {item.Name}");
+                });
+            }
+
         }
     }
 }
