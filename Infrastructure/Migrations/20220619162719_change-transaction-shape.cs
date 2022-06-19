29ce300f519f12adeaa3eac00f5dee5e0584d0b5
vitconkue <voan0935995700@gmail.com>
2022-06-19 23:41:04 +0700

    new transaction models

---
+++ Infrastructure/Migrations/20220619162719_change-transaction-shape.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class changetransactionshape : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AmountOfReferentialAssetBeforeCreatingTransaction",
+                table: "SingleAssetTransactions",
+                newName: "ValueOfReferentialAssetBeforeCreatingTransaction");
+
+            migrationBuilder.RenameColumn(
+                name: "AmountOfReferentialAssetBeforeCreatingTransaction",
+                table: "InvestFundTransactions",
+                newName: "ValueOfReferentialAssetBeforeCreatingTransaction");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "AmountOfDestinationAfterCreatingTransactionInSpecificUnit",
+                table: "SingleAssetTransactions",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "AmountOfSourceAssetAfterCreatingTransactionInSpecificUnit",
+                table: "SingleAssetTransactions",
+                type: "numeric",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AmountOfDestinationAfterCreatingTransactionInSpecificUnit",
+                table: "SingleAssetTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "AmountOfSourceAssetAfterCreatingTransactionInSpecificUnit",
+                table: "SingleAssetTransactions");
+
+            migrationBuilder.RenameColumn(
+                name: "ValueOfReferentialAssetBeforeCreatingTransaction",
+                table: "SingleAssetTransactions",
+                newName: "AmountOfReferentialAssetBeforeCreatingTransaction");
+
+            migrationBuilder.RenameColumn(
+                name: "ValueOfReferentialAssetBeforeCreatingTransaction",
+                table: "InvestFundTransactions",
+                newName: "AmountOfReferentialAssetBeforeCreatingTransaction");
+        }
+    }
+}
