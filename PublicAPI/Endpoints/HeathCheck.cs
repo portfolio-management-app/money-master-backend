c109d211490fe879b560f410e0c3ead6391d008d
vitconkue <voan0935995700@gmail.com>
2022-06-23 23:29:27 +0700

    fix lack http method in health check

--- PublicAPI/Endpoints/HeathCheck.cs
+++ PublicAPI/Endpoints/HeathCheck.cs
@@ -6,6 +6,7 @@ namespace PublicAPI.Endpoints
     public class HeathCheck: EndpointBaseSync.WithoutRequest.WithActionResult
     {
         [Route("healthCheck")]
+        [HttpGet]
         public override ActionResult Handle()
         {
             return Ok("The service is up and running."); 
